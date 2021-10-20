decc0a73e4a0da07a3c65fa7493457dff2e1fa6e
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-20 11:23:59 +0530

    modified controller class which is fetching values from properties file

--- limits-service/src/main/java/com/learn/microservice/limitsservice/controller/LimitController.java
+++ limits-service/src/main/java/com/learn/microservice/limitsservice/controller/LimitController.java
@@ -1,15 +1,19 @@
 package com.learn.microservice.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.microservice.limitsservice.bean.Limits;
+import com.learn.microservice.limitsservice.config.LimitConfig;
 
 @RestController
 public class LimitController {
+	@Autowired
+	private LimitConfig limitConfig;
 	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		return new Limits(limitConfig.getMinimum(),limitConfig.getMaximum());
 	}
 }
