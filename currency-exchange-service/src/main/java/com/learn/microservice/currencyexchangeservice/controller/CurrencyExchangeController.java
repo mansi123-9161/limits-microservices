de26afbf74d96b21805cf75995ba0bf988e64b5c
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-25 12:37:05 +0530

    added dynamic port to run multiple instance use -DServer.port

--- currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.learn.microservice.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,13 @@ import com.learn.microservice.currencyexchangeservice.entity.CurrencyExchange;
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	private Environment environment;
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(40));
+		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(40));
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 	}
 }
