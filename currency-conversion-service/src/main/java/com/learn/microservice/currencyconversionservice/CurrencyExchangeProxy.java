73daefd7cb66bddc8a50c3bfae3bdf0930760c82
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-27 09:54:15 +0530

    load balancing with eureka and feign

--- currency-conversion-service/src/main/java/com/learn/microservice/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/learn/microservice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.learn.microservice.currencyconversionservice.entity.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange",url = "localhost:8000")
+/*@FeignClient(name = "currency-exchange",url = "localhost:8000")*/
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
