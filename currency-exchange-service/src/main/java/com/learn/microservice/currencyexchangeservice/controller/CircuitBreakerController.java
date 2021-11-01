2fcfa1dffd5a70310a5dd713ac6b804ad2058cc7
mansi123-9161 <mansichourasia100@gmail.com>
2021-11-01 11:25:24 +0530

    adding retry method in resilence4j with customized max attempts

--- currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.learn.microservice.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,12 @@ import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
-	
+	private Logger logger=LoggerFactory.getLogger(CircuitBreakerController.class);
 	@GetMapping("/sampleMapping")
-	@Retry(name = "default")
+	@Retry(name = "sampleApi")
 	public String CircuitApi(){
 		/* by default it will retry 3 times */
+		logger.info("sample call recieved");
 		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://8089/spring", String.class);
 		return entity.getBody();
 	}
