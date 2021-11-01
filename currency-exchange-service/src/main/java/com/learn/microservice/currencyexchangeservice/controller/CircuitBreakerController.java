734a268d90f2dae1fa4d0d767bce48651d49d746
mansi123-9161 <mansichourasia100@gmail.com>
2021-11-01 11:11:21 +0530

    adding retry method in resilence4j

--- currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,14 +1,21 @@
 package com.learn.microservice.currencyexchangeservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 	
 	@GetMapping("/sampleMapping")
+	@Retry(name = "default")
 	public String CircuitApi(){
-		return "Sample API";
+		/* by default it will retry 3 times */
+		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://8089/spring", String.class);
+		return entity.getBody();
 	}
 
 }
